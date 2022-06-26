efe667677791f03979a5a853bb3de85d9e9627c9
Ascendus <ascendus.dev@gmail.com>
2022-06-26 21:46:08 +1200

    feat: added link to header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Container } from "../components/Container";
 import { Header } from "../components/Header";
 import { Icon } from "../components/Icon";
+import Link from "next/link";
 import type { NextPage } from "next";
 import { NextPageProps } from "../lib/types";
 
@@ -10,9 +11,11 @@ const Home: NextPage<NextPageProps> = ({ name }): JSX.Element => {
     return <Container>
         <Header title="A team comprised of 20 individuals" description="Servicing popular brands, and serving over 300,000 registered users and counting." buttons={[
             <div key={1} className="mt-[1.875em] justify-center items-center flex flex-wrap mb-[0.9375em] hover:-translate-y-1 transition-all duration-[1s] ease-in-out">
-                <a href="/demos/libris/docs" className="bg-[#fff] text-[#00c6ff] text-[1em] font-bold rounded-[1.75em] py-[0.9em] px-[2.14285em] hover:opacity-[.9] transition-all duration-[.3s] ease" style={{
-                    margin: "0 0.375em 0"
-                }}>Work with us</a>
+                <Link href="/discord" passHref>
+                    <a className="bg-[#fff] text-[#00c6ff] text-[1em] font-bold rounded-[1.75em] py-[0.9em] px-[2.14285em] hover:opacity-[.9] transition-all duration-[.3s] ease" style={{
+                        margin: "0 0.375em 0"
+                    }}>Work with us</a>
+                </Link>
             </div>
         ]} />
         <section id="features" className="relative -mt-[5em] px-[3vw] overflow-hidden">
@@ -26,7 +29,7 @@ const Home: NextPage<NextPageProps> = ({ name }): JSX.Element => {
                                     <p style={{ margin: "0 0 1em" }}>Over 595 updates across all services provided in 2018-2022</p>
                                 </div>
                                 <div className="px-[1.5em] items-center flex flex-wrap mb-[0.9375em]">
-                                <a href="#" className="flex flex-row items-center gap-x-1 text-[#00c6ff] transition-colors duration-[.3s] ease font-semibold hover:text-[#424b5f]" style={{ margin: "0 0.75em 0.75em 0" }}>Learn More <Icon icon={faArrowRight} className="h-3 pt-[0.2rem]" /></a>
+                                    <a href="#" className="flex flex-row items-center gap-x-1 text-[#00c6ff] transition-colors duration-[.3s] ease font-semibold hover:text-[#424b5f]" style={{ margin: "0 0.75em 0.75em 0" }}>Learn More <Icon icon={faArrowRight} className="h-3 pt-[0.2rem]" /></a>
                                 </div>
                             </div>
                         </div>
@@ -48,7 +51,7 @@ const Home: NextPage<NextPageProps> = ({ name }): JSX.Element => {
                                     <p style={{ margin: "0 0 1em" }}>Worth over $90,000</p>
                                 </div>
                                 <div className="px-[1.5em] items-center flex flex-wrap mb-[0.9375em]">
-                                <a href="#" className="flex flex-row items-center gap-x-1 text-[#00c6ff] transition-colors duration-[.3s] ease font-semibold hover:text-[#424b5f]" style={{ margin: "0 0.75em 0.75em 0" }}>Learn More <Icon icon={faArrowRight} className="h-3 pt-[0.2rem]" /></a>
+                                    <a href="#" className="flex flex-row items-center gap-x-1 text-[#00c6ff] transition-colors duration-[.3s] ease font-semibold hover:text-[#424b5f]" style={{ margin: "0 0.75em 0.75em 0" }}>Learn More <Icon icon={faArrowRight} className="h-3 pt-[0.2rem]" /></a>
                                 </div>
                             </div>
                         </div>
